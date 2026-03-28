0fbf3f7efc0cc33ade10e803d3dbbbe2c37b924d
eleukanjanbo-batek <eleukanjanbo@gmail.com>
2026-03-28 08:41:00 +0300

    Sayt janalandi

--- app.py
+++ app.py
@@ -7,12 +7,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+
 # Render'dagi Settings -> Environment qismidan oladi
 app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///online_kurs.db')
+
+# 🔥 SHU JOY O‘ZGARTIRILDI (absolute path)
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, 'instance', 'online_kurs.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
 
 # --- MODELLAR (O'zgarishsiz qoladi) ---
 class Course(db.Model):
@@ -22,35 +29,38 @@ class Course(db.Model):
     price = db.Column(db.String(50), nullable=False)
     applications = db.relationship('Application', backref='course_rel', lazy=True, cascade="all, delete-orphan")
 
+
 class Application(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(20), nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
 
+
 class AdminProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(100), default="Rustamov Davron")
-    bio = db.Column(db.Text, default="IT-Academy asoschisi va rahbari")
+    full_name = db.Column(db.String(100), default="Eleukanov Janbolat")
+    bio = db.Column(db.Text, default="IT-Academy tiykarshisi ham basligi")
     password = db.Column(db.String(200), nullable=False)
+
 
 # --- BAZANI VA ADMINNI TO'G'RI SOZLASH ---
 with app.app_context():
     db.create_all()
-    
+
     # Render'dagi parolni o'zgaruvchidan olamiz
     target_pw = os.getenv('ADMIN_PASSWORD', 'admin123')
     admin = AdminProfile.query.first()
-    
+
     if not admin:
         hashed_pw = generate_password_hash(target_pw)
         db.session.add(AdminProfile(password=hashed_pw))
-        print("Yangi admin yaratildi!")
+        print("Jana admin jaratildi!")
     else:
         # Har safar sayt yurganda parolni yangilab turadi (Render xatolarini tuzatish uchun)
         admin.password = generate_password_hash(target_pw)
-        print("Admin paroli yangilandi!")
-    
+        print("Admin paroli janalandi!")
+
     db.session.commit()
 
 # --- YO'NALISHLAR (O'zgarishsiz qoladi) ---
